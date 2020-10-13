91ffd99308d7a4108d37fceac4c8a2b174bc305e
UBC Student <wchoi1@valdes.students.cs.ubc.ca>
2020-10-12 21:55:16 -0700

    first draft of secure connect

--- checksec.c
+++ checksec.c
@@ -30,11 +30,15 @@ void init_ssl() {
 }
 
 void *read_user_input(void *arg) {
+  printf("\nhere\n");
   SSL *ssl = arg;
 
   char buf[BUFFER_SIZE];
+  printf("\nheresdf\n");
   size_t n;
+  printf("\nher3e\n");
   while (fgets(buf, sizeof(buf) - 1, stdin)) {
+    // printf("her123123e\n");
     /* Most text-based protocols use CRLF for line-termination. This
        code replaced a LF with a CRLF. */
     n = strlen(buf);
@@ -43,7 +47,6 @@ void *read_user_input(void *arg) {
     
     /* TODO Send message */
   }
-
   /* TODO EOF in stdin, shutdown the connection */
   
   return 0;
@@ -54,16 +57,40 @@ void secure_connect(const char* hostname, const char *port) {
   char buf[BUFFER_SIZE];
 
   /* TODO Establish SSL context and connection */
+  const SSL_METHOD* ssl_method = TLSv1_2_client_method();
+  SSL_CTX* ctx = SSL_CTX_new(ssl_method);
+  SSL* ssl = NULL;
+  X509 *certificate;
+  BIO* bio = BIO_new_ssl_connect(ctx);
+  BIO_get_ssl(bio, &ssl);
+  certificate = SSL_get_peer_certificate(ssl);
+  BIO_set_conn_hostname(bio, hostname);
+  BIO_do_connect(bio);
   /* TODO Print stats about connection */
-
   /* Create thread that will read data from stdin */
   pthread_t thread;
   pthread_create(&thread, NULL, read_user_input, ssl);
   pthread_detach(thread);
-  
+  size_t mk_len = 1;
+  size_t num_copied = 0;
+  unsigned char* mk = malloc(mk_len);
+  SSL_SESSION* session = SSL_get_session(ssl);
+  do{
+    mk_len *= 2;
+    mk = (char*)realloc(mk, mk_len);
+    num_copied = SSL_SESSION_get_master_key(session, mk, mk_len);
+  } while(mk_len > num_copied);
+  for (int i = 0; i < num_copied; i++)
+  {
+    printf("hello: %02X", mk[i]);
+  }
   fprintf(stderr, "\nType your message:\n\n");
 
   /* TODO Receive messages and print them to stdout */
+  // while(buf) {
+    
+  // }
+  printf("sadfsdfsdf");
 }
 
 int main(int argc, char *argv[]) {
